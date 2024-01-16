e97877c22db31621305800615f0a050841e4c612
Arjun Atpadkar <arjunatapadkar94@gmail.com>
2024-01-16 13:31:07 +0530

    user picker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [chips, setChips] = useState([]);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const allItems = ['Emma Wilson', 'Abigail Cooper', 'Evelyn Adams', 'Mason Turner' ,'Noah Anderson', 'Ava Martinez', 'Lucas Taylor', 'Jackson Brown', 'Aiden Harris', 'Logan Mitchell']; 
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleItemClick = (item) => {
+    setChips([...chips, item]);
+    setSearchTerm('');
+  };
+
+  const handleChipRemove = (chip) => {
+    setChips(chips.filter((item) => item !== chip));
+  };
+
+  const handleInputFocus = () => {
+    setIsInputFocused(true);
+  };
+
+
+  const filteredItems = allItems.filter(
+    (item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase()) && !chips.includes(item)
+  );
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-full flex flex-col justify-center items-center mt-10 gap-20' >
+
+      <h1 className='text-3xl font-bold'>Pick User</h1>
+
+      <div className='flex gap-2 border-b-2 border-violet-700 w-full items-center justify-center px-32'>
+
+        <div className='flex flex-wrap gap-4 py-2'>
+          {chips.map((chip) => (
+            <div key={chip} className='bg-slate-200 px-4 py-1 rounded-full'>
+              {chip} <span className='ml-4' onClick={() => handleChipRemove(chip)}>X</span>
+            </div>
+          ))}
+        </div>
+        <div>
+
+          <input
+          type="text"
+          value={searchTerm}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          // onBlur={() =>setIsInputFocused(false)}
+          placeholder="Add new user"
+          className=' border-none outline-none'
+          />
+          <ul className={ isInputFocused ? 'block absolute border p-5 shadow-md  z-10' : 'hidden' }>
+            {filteredItems.map((item) => (
+              <li key={item}  onClick={() => {
+                    handleItemClick(item);
+                    setIsInputFocused(false);
+                  }}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      
     </div>
   );
 }
 
 export default App;
+
+
+
+
